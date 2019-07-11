9e40a1f7163b254950c58d9f8cee030c0b4d45d1
Kseniia Anishchenko <kseninavoi@gmail.com>
2019-07-11 18:32:09 +0500

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
+import HomePage from "./pages/homepage/homepage";
 
-function App() {
-  return (
-    <div>
-      Hello World!
-    </div>
-  );
+const App = () => {
+  return <HomePage />;
 }
 
 export default App;
---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import DirectoryMenu from "../../components/directory-menu/directory-menu";
+import "./homepage.styles.scss";
+
+const HomePage = () => {
+  return <div className="homepage">
+    <DirectoryMenu />
+  </div>
+};
+
+export default HomePage;
