3ad5c698af2bc5ec8352d73d9f850212da1ceb78
Kseniia Anishchenko <kseninavoi@gmail.com>
2019-07-14 10:11:01 +0500

    create sign in components, formInput, customButton

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from "react-router";
 import HomePage from "./pages/homepage/homepage";
 import Shop from "./pages/shop/shop";
 import Header from "./components/header/header";
+import SignIn from "./components/sign-in/sign-in";
 
 import './App.css';
 
@@ -13,6 +14,7 @@ const App = () => {
     <Switch>
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={Shop}/>
+      <Route exact path='/signin' component={SignIn}/>
     </Switch>
   </div>;
 }
---
+++ src/pages/sing-in-page/sign-in-page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in";
+
+const SignInPage = () => {
+  return <div>
+  <SignIn />
+  </div>
+}
+
+export default SignInPage;
