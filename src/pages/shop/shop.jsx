be47ea913a26ba9fcd016da3a65fc6265869b6c8
Kseniia Anishchenko <kseninavoi@gmail.com>
2019-08-12 15:46:50 -0700

    fix bag

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -5,7 +5,7 @@ import {createStructuredSelector} from 'reselect';
 
 import CollectionOverview from '../../components/collections-overview/collections-overview';
 import CollectionPage from '../collection/collection';
-import {selectIsFetching} from '../../redux/shop/shop.selectors';
+import {selectIsFetching, selectIsCollectionLoading} from '../../redux/shop/shop.selectors';
 import {fetchCollectionAsinc} from '../../redux/shop/shop.actions';
 import WithSpinner from '../../components/with-spinner/with-spinner';
 
@@ -20,16 +20,17 @@ class Shop extends React.Component {
   }
 
   render() {
-    const {match, isFetching} = this.props;
+    const {match, isFetching, isCollectionsLoading} = this.props;
     return <div className='shop-page'>
       <Route exact path={`${match.path}`} render={props => <CollectionOverviewWithSpinner isLoading={isFetching} {...props}/>} />
-      <Route path={`${match.path}/:collectionId`} render={props => <CollectionPageWithSpinner isLoading={isFetching} {...props} />} />
+      <Route path={`${match.path}/:collectionId`} render={props => <CollectionPageWithSpinner isLoading={!isCollectionsLoading} {...props} />} />
     </div>
   }
 }
 
 const mapStateToProps = state => createStructuredSelector({
-  isFetching: selectIsFetching
+  isFetching: selectIsFetching,
+  isCollectionsLoading: selectIsCollectionLoading
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -22,3 +22,8 @@ export const selectCollection = collectionUrlParam => createSelector(
   [selectShopCollections],
   collections => collections ? collections[collectionUrlParam] : null
 )
+
+export const selectIsCollectionLoading = createSelector(
+  [selectShop],
+  shop => !!shop.collections
+)
